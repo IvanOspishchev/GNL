f9ca535109b9e7a032aa0bd2bef90843664ffb7e
IvanOspishchev <35837450+IvanOspishchev@users.noreply.github.com>
2018-12-24 14:25:22 +0300

    Add files via upload

--- get_next_line.c
+++ get_next_line.c
@@ -6,14 +6,14 @@
 /*   By: nparker <nparker@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/19 18:40:14 by nparker           #+#    #+#             */
-/*   Updated: 2018/12/22 13:30:39 by nparker          ###   ########.fr       */
+/*   Updated: 2018/12/24 13:43:02 by nparker          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include "libft.h"
 
-int				ft_new_line(char **str, char **line, int fd, int read_fd)
+int				create_line(char **str, char **line, int fd, int read_fd)
 {
 	char		*tmp;
 	int			len;
@@ -40,29 +40,38 @@ int				ft_new_line(char **str, char **line, int fd, int read_fd)
 	return (1);
 }
 
+int				read_line(const int fd, char **str, char *buff, int read_fd)
+{
+	char		*temp;
+
+	while ((read_fd = read(fd, buff, BUFF_SIZE)) > 0)
+	{
+		buff[read_fd] = '\0';
+		if (str[fd] == NULL)
+			str[fd] = ft_strnew(1);
+		temp = ft_strjoin(str[fd], buff);
+		free(str[fd]);
+		str[fd] = temp;
+		if (ft_strchr(buff, '\n'))
+			break ;
+	}
+	return (read_fd);
+}
+
 int				get_next_line(const int fd, char **line)
 {
 	static char	*str[64536];
-	char		buf[BUFF_SIZE + 1];
-	char		*tmp;
+	char		buff[BUFF_SIZE + 1];
 	int			read_fd;
 
+	read_fd = 0;
 	if (fd == -1 || line == NULL)
 		return (-1);
-	while ((read_fd = read(fd, buf, BUFF_SIZE)) > 0)
-	{
-		buf[read_fd] = '\0';
-		if (str[fd] == NULL)
-			str[fd] = ft_strnew(1);
-		tmp = ft_strjoin(str[fd], buf);
-		free(str[fd]);
-		str[fd] = tmp;
-		if (ft_strchr(buf, '\n'))
-			break ;
-	}
+	if (read_line(fd, str, buff, read_fd) < 0)
+		return (-1);
 	if (read_fd < 0)
 		return (-1);
 	else if (read_fd == 0 && (str[fd] == NULL || str[fd][0] == '\0'))
 		return (0);
-	return (ft_new_line(str, line, fd, read_fd));
+	return (create_line(str, line, fd, read_fd));
 }
